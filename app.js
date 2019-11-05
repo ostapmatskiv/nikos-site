70e032623a0975df6d62080f4996a2f6dbe6ce6b
Ostap Matskiv <levso7@gmail.com>
2019-11-05 17:13:22 +0200

    cart

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-// document.body.style.background = 'green';
-// if(confirm('delete red bg?')) {
-// 	document.body.style.background = '';
-// }
+// Отримати всі посилання які відкривають корзину за класом open-cart
+var cartBtn = document.getElementsByClassName('open-cart');
+for (var i = 0; i < cartBtn.length; i++) {
+	cartBtn[i].onclick = openCart; //кожному елементу присвоїти ф-цію openCart
+}
+//іконці "хрестик" в корзині присвоїти подію "Закрити корзину"
+document.querySelector('#cart div > i.fa-times-circle').onclick = closeCart;
+
+function openCart () {
+	event.preventDefault();
+	cart.style.display = 'flex';
+}
+function closeCart () {
+	cart.style.display = 'none';
+}
