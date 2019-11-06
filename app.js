26800f9322ed787a174ad751133504c6ac864061
Ostap Matskiv <levso7@gmail.com>
2019-11-06 12:56:20 +0200

    js base functions

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var cartBtn = document.getElementsByClassName('open-cart');
 for (var i = 0; i < cartBtn.length; i++) {
 	cartBtn[i].onclick = openCart; //кожному елементу присвоїти ф-цію openCart
 }
+// Отримати всі кнопки які є в section.rooms article
+var buyBtn = document.querySelectorAll('section.rooms article button');
+for (var i = 0; i < buyBtn.length; i++) {
+	buyBtn[i].onclick = addProductToCart; //кожному елементу присвоїти ф-цію openCart
+}
 //іконці "хрестик" в корзині присвоїти подію "Закрити корзину"
 document.querySelector('#cart div > i.fa-times-circle').onclick = closeCart;
 
@@ -12,4 +17,7 @@ function openCart () {
 }
 function closeCart () {
 	cart.style.display = 'none';
+}
+function addProductToCart() {
+	openCart ();
 }
