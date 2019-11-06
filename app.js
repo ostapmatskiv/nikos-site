a8440b931c12e419c3795264f51441d385fdca78
Ostap Matskiv <levso7@gmail.com>
2019-11-06 16:06:30 +0200

    js base addProductToCart

--- app.js
+++ app.js
@@ -18,6 +18,40 @@ function openCart () {
 function closeCart () {
 	cart.style.display = 'none';
 }
+
+var nextI = 1;
 function addProductToCart() {
-	openCart ();
+	var price = this.previousElementSibling.innerText;
+	var name = this.parentElement.firstElementChild.innerText;
+
+	var tr = document.createElement('tr'); // стаорюємо рядок
+
+	var td1 = document.createElement('td'); // створюємо 1-у клітинку
+	td1.innerText = nextI++; // клітинці 1 задаємо текст nextI
+	tr.appendChild(td1); // додаємо клітинку до рядка
+
+	var td2 = document.createElement('td'); // створюємо 2-у клітинку
+	td2.innerText = name; // клітинці 2 задаємо текст name
+	tr.appendChild(td2); // додаємо клітинку до рядка
+
+	var td3 = document.createElement('td'); // створюємо 3-у клітинку
+	td3.innerText = price; // клітинці 3 задаємо текст
+	tr.appendChild(td3); // додаємо клітинку до рядка
+
+	var td4 = document.createElement('td'); // створюємо 4-у клітинку
+	td4.innerText = '1'; // клітинці 4 задаємо текст
+	tr.appendChild(td4); // додаємо клітинку до рядка
+
+	var td5 = document.createElement('td'); // створюємо 5-у клітинку
+	td5.innerText = price; // клітинці 5 задаємо текст "$50000"
+	tr.appendChild(td5); // додаємо клітинку до рядка
+
+	var td6 = document.createElement('td'); // створюємо 6-у клітинку
+	td6.innerHTML = '<button>x</button>'; // клітинці 6 задаємо кнопку
+	tr.appendChild(td6); // додаємо клітинку до рядка
+
+	var table = cart.querySelector('table tbody'); // знаходимо тіло таблиці у cart
+	table.appendChild(tr); // додаємо до таблиці новостворений рядок
+	
+	openCart (); // виводимо корзину
 }
